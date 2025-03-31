b2ece6b3fea65224812b36ab605ff6d051803d07
Artem.Mokeev <artmokeev274@gmail.com>
2025-03-31 16:24:28 +0300

    added uploads route

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,17 @@ import registerRoute from "./routes/register.js";
 import loginRoute from "./routes/login.js";
 import userRoute from "./routes/user.js";
 import postsRoute from "./routes/posts.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import uploadRoute from "./routes/upload.js";
 
 dotenv.config();
 const app = e();
 const PORT = process.env.PORT;
+
+// Определяем текущую директорию
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Инициализация базы данных
 initializeDatabase();
@@ -20,6 +27,15 @@ app.use("/", registerRoute);
 app.use("/", loginRoute);
 app.use("/", userRoute);
 app.use("/", postsRoute);
+app.use("/", uploadRoute);
+
+// Раздача статических файлов из папки uploads
+app.use("/uploads", e.static(path.join(__dirname, "uploads")));
+
+// Обработка неизвестных маршрутов
+app.use((req, res, next) => {
+	res.status(404).json({ error: "Page not found" });
+});
 
 app.listen(PORT, (err) => {
 	if (err) {
