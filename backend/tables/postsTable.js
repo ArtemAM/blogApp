facd3d390cb73780dadf13d1a2cc0865bf1bc25d
Artem.Mokeev <artmokeev274@gmail.com>
2025-03-31 17:22:53 +0300

    turned into a promise

--- backend/tables/postsTable.js
+++ backend/tables/postsTable.js
@@ -1,24 +1,26 @@
-const createPostsTable = (db) => {
-	db.run(
-		`CREATE TABLE IF NOT EXISTS posts (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user INTEGER NOT NULL,
-        title TEXT NOT NULL,
-        text TEXT NOT NULL,
-        tags TEXT DEFAULT '[]',
-        imageUrl TEXT,
-        viewsCount INTEGER DEFAULT 0,
-        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (user) REFERENCES users(id) ON DELETE CASCADE
-    )`,
-		(err) => {
-			if (err) {
-				console.error("Error creating posts table:", err);
-			} else {
-				console.log("Posts table created or already exists");
+export default (db) => {
+	return new Promise((resolve, reject) => {
+		db.run(
+			`
+          CREATE TABLE IF NOT EXISTS posts (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              user INTEGER NOT NULL,
+              title TEXT NOT NULL,
+              text TEXT NOT NULL,
+              tags TEXT DEFAULT '[]',
+              imageUrl TEXT,
+              viewsCount INTEGER DEFAULT 0,
+              createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+              FOREIGN KEY (user) REFERENCES users(id)
+          )
+          `,
+			(err) => {
+				if (err) {
+					return reject(err);
+				}
+				console.log("Posts table created");
+				resolve();
 			}
-		}
-	);
+		);
+	});
 };
-
-export default createPostsTable;
--- backend/tables/usersTable.js
+++ backend/tables/usersTable.js
@@ -1,21 +1,22 @@
-const createUserTable = (db) => {
-	db.run(
-		`CREATE TABLE IF NOT EXISTS users (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          fullName TEXT NOT NULL,
-          email TEXT NOT NULL UNIQUE,
-          passwordHash TEXT NOT NULL,
-          avatarUrl TEXT,
-          createdAt DATETIME DEFAULT CURRENT_TIMESTAMP
-      )`,
-		(err) => {
-			if (err) {
-				console.error("Error creating users table:", err);
-			} else {
-				console.log("Users table created or already exists");
+export default (db) => {
+	return new Promise((resolve, reject) => {
+		db.run(
+			`CREATE TABLE IF NOT EXISTS users (
+						id INTEGER PRIMARY KEY AUTOINCREMENT,
+						fullName TEXT NOT NULL,
+						email TEXT NOT NULL UNIQUE,
+						passwordHash TEXT NOT NULL,
+						avatarUrl TEXT,
+						createdAt DATETIME DEFAULT CURRENT_TIMESTAMP
+				)`,
+			(err) => {
+				if (err) {
+					return reject(err);
+				} else {
+					console.log("Users table created");
+					resolve();
+				}
 			}
-		}
-	);
+		);
+	});
 };
-
-export default createUserTable;
