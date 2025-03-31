c8c41f52a7ac63a68d3800a9a5d153b160764fa5
Artem.Mokeev <artmokeev274@gmail.com>
2025-03-31 16:17:57 +0300

    added upload route

---
+++ backend/routes/upload.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import upload from "../middlewares/uploadMiddleware.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
+
+const router = Router();
+
+// Маршрут для загрузки изображений
+router.post("/upload", authMiddleware, upload.single("image"), (req, res) => {
+	try {
+		// Если файл был загружен, возвращаем путь к файлу
+		if (!req.file) {
+			return res.status(400).json({ error: "No file uploaded" });
+		}
+
+		const imageUrl = `/uploads/${req.file.filename}`;
+		res.status(201).json({ message: "File uploaded successfully", imageUrl });
+	} catch (err) {
+		console.error("Error uploading file:", err);
+		res.status(500).json({ error: "Failed to upload file" });
+	}
+});
+
+export default router;
