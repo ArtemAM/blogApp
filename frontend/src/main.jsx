70869349679e26630e774c10379b489a66e36753
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-01 14:56:36 +0300

    feat: add parallel fetching fetchPosts

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,9 @@ import { Provider } from 'react-redux';
 import store from './redux/store.js';
 import './index.css';
 import App from './App.jsx';
+import { fetchPosts } from './redux/slices/posts.slices.js';
+
+store.dispatch(fetchPosts());
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
