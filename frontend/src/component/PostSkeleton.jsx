82d4b261b997644871c5cea0a84438d869e31139
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-01 17:32:47 +0300

    refactor: change structure post skeleton

--- frontend/src/component/PostSkeleton.jsx
+++ frontend/src/component/PostSkeleton.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {
   Card,
-  CardHeader,
   CardContent,
   CardMedia,
+  CardHeader,
   Skeleton,
   Box,
+  Typography,
 } from '@mui/material';
 
 function PostSkeleton() {
@@ -17,27 +18,31 @@ function PostSkeleton() {
         '&:hover .post-actions': { opacity: 1 },
       }}
     >
-      <CardMedia>
+      <CardMedia height={250}>
         <Skeleton variant="rectangular" height={250} />
       </CardMedia>
       <CardHeader
         avatar={<Skeleton variant="circular" width={40} height={40} />}
         title={<Skeleton variant="text" width="20%" />}
         subheader={<Skeleton variant="text" width="40%" />}
+        sx={{ paddingBottom: 0 }}
       />
       <CardContent>
-        <Skeleton variant="text" width="50%" />
+        <Typography variant="h6" gutterBottom>
+          <Skeleton variant="text" width="50%" />
+        </Typography>
         <Box sx={{ display: 'flex', gap: 1, flexWrap: 'wrap' }}>
-          <Skeleton variant="rounded" width={60} height={20} />
-          <Skeleton variant="rounded" width={60} height={20} />
-          <Skeleton variant="rounded" width={60} height={20} />
+          {[...Array(3)].map((tag, index) => (
+            <Skeleton key={index} width={40} height={30} />
+          ))}
         </Box>
-        <Skeleton
-          variant="rounded"
-          width={10}
-          height={10}
-          sx={{ marginTop: 3 }}
-        />
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          sx={{ display: 'flex', gap: 0.5, marginTop: 1 }}
+        >
+          <Skeleton variant="text" width="5%" />
+        </Typography>
       </CardContent>
     </Card>
   );
