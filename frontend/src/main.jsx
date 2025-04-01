2c6dc1f3dd88e072b1f608228acc8b80af31fb31
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-01 17:31:48 +0300

    feat: add parallel fetching tags

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,8 +5,10 @@ import store from './redux/store.js';
 import './index.css';
 import App from './App.jsx';
 import { fetchPosts } from './redux/slices/posts.slice.js';
+import { fetchTags } from './redux/slices/tags.slice.js';
 
 store.dispatch(fetchPosts());
+store.dispatch(fetchTags());
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
