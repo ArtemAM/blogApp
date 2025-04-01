fd785f0e6947be46da7e94cc410a7b3b975d606f
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-01 11:16:35 +0300

    feat: create AuthorInfo component

---
+++ frontend/src/component/AuthorInfo.jsx
@@ -0,0 +1,15 @@
+import { CardHeader, Avatar } from '@mui/material';
+import React from 'react';
+
+function AuthorInfo({ avatar, author, date }) {
+  return (
+    <CardHeader>
+      avatar={<Avatar src={avatar} alt={author} />}
+      title={author}
+      subheader={date}
+      sx={{ paddingBottom: 0 }}
+    </CardHeader>
+  );
+}
+
+export default AuthorInfo;
