ea455b8732d6ad21f84178ddfd7d74ea3b49b9ad
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-01 11:57:00 +0300

    feat: render Home component in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import { Container } from '@mui/material';
 import Header from './component/Header';
+import Home from './pages/Home';
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Container maxWidth="lg">
+        <Home />
+      </Container>
+    </>
+  );
 }
 
 export default App;
