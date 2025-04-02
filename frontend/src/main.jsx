12581ad45bc642444fa82d65816aeef4a48ddfa1
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-02 13:35:56 +0300

    feat: integrate react router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
 import './index.css';
-import App from './App.jsx';
 import { fetchPosts } from './redux/slices/posts.slice.js';
 import { fetchTags } from './redux/slices/tags.slice.js';
+import { RouterProvider } from 'react-router-dom';
+import router from './router.jsx';
 
 store.dispatch(fetchPosts());
 store.dispatch(fetchTags());
@@ -13,7 +14,7 @@ store.dispatch(fetchTags());
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>,
 );
