0b97e59000f1345fe918bf40fc4de702d734fc85
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-02 17:42:01 +0300

    feat: add login reducer

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { postsReducer } from './slices/posts.slice';
 import { tagsReducer } from './slices/tags.slice';
 import api from '../api/api';
+import { loginReducer } from './slices/login.slice';
 
 const extraArgument = {
   api,
@@ -11,6 +12,7 @@ const store = configureStore({
   reducer: {
     posts: postsReducer,
     tags: tagsReducer,
+    login: loginReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
