34c3e0650fb4023455e21f97035d0d4eb1281f6b
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-02 17:55:22 +0300

    feat: add route for login page

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import PostView from './pages/PostView';
 import Home from './pages/Home';
 import App from './App';
+import Login from './pages/Login';
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
       {
         path: 'posts/:id',
         element: <PostView />,
+      },
+      {
+        path: '/login',
+        element: <Login />,
       },
     ],
   },
