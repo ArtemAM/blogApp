7047a3d4e3569cbac1afa556cbd98b16667dc699
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-02 09:16:07 +0300

    chore: normalize line endings to LF

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
 import { Container } from '@mui/material';
 import Header from './component/Header';
 import Home from './pages/Home';
+import PostView from './component/PostView';
 
 function App() {
+  const post = {
+    image: '../public/post.png',
+    avatar: 'https://via.placeholder.com/40',
+    author: 'John Doe',
+    date: 'April 1, 2025',
+    title: 'Sample Post Title',
+    tags: ['react', 'javascript', 'webdev'],
+    text: 'This is the full text of the post. It contains detailed information about the topic.',
+    views: 123,
+  };
   return (
     <>
       <Header />
       <Container maxWidth="lg">
-        <Home />
+        {/* <Home /> */}
+        <PostView {...post} />
       </Container>
     </>
   );
