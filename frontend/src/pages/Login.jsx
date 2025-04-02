25f55e26f50c191fc21108d383f23606672151a9
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-02 17:52:06 +0300

    feat: implement login form with react-hook-form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,13 @@
 import { Box, TextField, Button, Typography } from '@mui/material';
+import { useForm } from 'react-hook-form';
 
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = () => {};
   return (
     <Box
       sx={{
@@ -15,7 +22,12 @@ function Login() {
       <Typography component="h1" variant="h5">
         Login
       </Typography>
-      <Box component="form" noValidate sx={{ mt: 3 }}>
+      <Box
+        component="form"
+        onSubmit={handleSubmit(onSubmit)}
+        noValidate
+        sx={{ mt: 3 }}
+      >
         <TextField
           margin="normal"
           required
@@ -25,6 +37,15 @@ function Login() {
           name="email"
           autoComplete="email"
           autoFocus
+          {...register('email', {
+            required: 'Email is required',
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+              message: 'Invalid email address',
+            },
+          })}
+          error={!!errors.email}
+          helperText={errors.email ? errors.email.message : ''}
         />
         <TextField
           margin="normal"
@@ -35,6 +56,15 @@ function Login() {
           type="password"
           id="password"
           autoComplete="current-password"
+          {...register('password', {
+            required: 'Password is required',
+            minLength: {
+              value: 6,
+              message: 'Password must be at least 6 characters long',
+            },
+          })}
+          error={!!errors.password}
+          helperText={errors.password ? errors.password.message : ''}
         />
 
         <Button
