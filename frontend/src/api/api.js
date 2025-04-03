bb0593bd27b1be6fd38e3cbb37d1345be86c193e
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-03 09:14:27 +0300

    feat: add request interceptor to include token in headers

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -5,6 +5,14 @@ const instance = axios.create({
   baseURL: API_URL,
 });
 
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = token;
+  }
+  return config;
+});
+
 const api = {
   getPosts: async () => {
     try {
