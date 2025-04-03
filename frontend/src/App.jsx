7aaff1df00a878f4d7b40e729a890f054f614e8e
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-03 11:26:21 +0300

    feat: fetch authenticated user on app initialization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
 import { Container } from '@mui/material';
 import Header from './component/Header';
 import { Outlet } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchAuthUser } from './redux/slices/login.slice';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchAuthUser());
+  }, [dispatch]);
   return (
     <>
       <Header />
