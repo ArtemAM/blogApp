d38945a5225be53bc1196c23356046a921614af4
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-04 18:01:57 +0300

    refactor: delete useEffect fetch data

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,15 +2,14 @@ import { Grid, Box, ButtonGroup, Button } from '@mui/material';
 import Tags from '../component/Tags';
 import Post from '../component/Post';
 import PostSkeleton from '../component/PostSkeleton';
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchPosts, postsSlice } from '../redux/slices/posts.slice';
-import { fetchTags, tagsSlice } from '../redux/slices/tags.slice';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { postsSlice } from '../redux/slices/posts.slice';
+import { tagsSlice } from '../redux/slices/tags.slice';
 import TagsSkeleton from '../component/TagsSkeleton';
 import { loginSlice } from '../redux/slices/login.slice';
 
 function Home() {
-  const dispatch = useDispatch();
   const [sortType, setSortType] = useState('new');
   const sortedPosts = useSelector((state) =>
     postsSlice.selectors.selectSortedPosts(state, sortType),
@@ -23,10 +22,6 @@ function Home() {
   const isLoadingTags = useSelector(
     tagsSlice.selectors.selectIsFetchTagsPending,
   );
-  useEffect(() => {
-    dispatch(fetchPosts());
-    dispatch(fetchTags());
-  }, [dispatch]);
   return (
     <Grid container spacing={2} alignItems="flex-start" sx={{ paddingTop: 4 }}>
       <Grid size={9}>
