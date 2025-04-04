cfffa7ebfc223a3805ff38666b7529944c28a553
Artem.Mokeev <artmokeev274@gmail.com>
2025-04-04 11:49:30 +0300

    feat: add post editing logic

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -21,12 +21,13 @@ import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 import { useSelector } from 'react-redux';
 import { loginSlice } from '../redux/slices/login.slice';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import api from '../api/api';
 
 function CreatePost() {
   const isAuth = useSelector(loginSlice.selectors.selectIsAuth);
   const navigate = useNavigate();
+  const { id } = useParams();
 
   const [title, setTitle] = useState('');
   const [tags, setTags] = useState([]);
@@ -39,7 +40,22 @@ function CreatePost() {
 
   useEffect(() => {
     if (!localStorage.getItem('token') && !isAuth) navigate('/');
-  }, [isAuth, navigate]);
+    const fetchData = async () => {
+      try {
+        const { title, text, tags, imageUrl } = await api.getPostById(id);
+        setTitle(title);
+        setContent(text);
+        setTags(tags);
+        setImage(imageUrl);
+        setFile(imageUrl);
+      } catch (error) {
+        console.error('Error fetching post data:', error);
+      }
+    };
+    if (id) {
+      fetchData();
+    }
+  }, [isAuth, navigate, id]);
 
   const options = useMemo(
     () => ({
@@ -67,7 +83,7 @@ function CreatePost() {
   const handleFileChange = async (e) => {
     const file = e.target.files[0];
     if (file) {
-      setFile(e.target.files[0]);
+      setFile(file.name);
     }
     const formData = new FormData();
     formData.append('image', file);
@@ -180,7 +196,7 @@ function CreatePost() {
             variant="outlined"
             onClick={() => fileInputRef.current.click()}
           >
-            {file ? file.name : 'Upload image'}
+            {file ? file : 'Upload image'}
           </Button>
           {file && (
             <IconButton onClick={handleCancelImage} sx={{ ml: 1 }}>
